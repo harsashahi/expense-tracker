5cf6a502cb7a4e2eb7e79b113c2485fcf298a963
harsa shahi <harshashahi511@gmail.com>
2025-07-09 09:17:24 +0545

    added HTML files with CSS

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "tracker"
+    'tracker'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'expensetracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'expense',
+        'USER': 'postgres',
+        'PASSWORD': 'Harsha123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -114,8 +118,21 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+import os
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_DIR = {
+    os.path.join(BASE_DIR , "public/static")
+}
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'public/static') 
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect , render
+from django.contrib.auth import logout
 
 # Create your views here.
+ 
+
+def index(request):
+    return render( request,'index.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')  
